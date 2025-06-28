92be9b1f1d7b7363916ee1630100c8d3cf426cd1
bishir-tm <bishirtijjani247@gmail.com>
2025-06-28 15:25:41 +0100

    feat: add github pages deployment

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: process.env.ELECTRON ? "./" : "/",
+  base: "/codeket-virtual-tours/",
   plugins: [react()],
 });
