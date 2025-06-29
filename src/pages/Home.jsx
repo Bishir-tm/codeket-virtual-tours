e59284d6f1fdf1f0c085cf47487a28fa45279e9c
bishir-tm <bishirtijjani247@gmail.com>
2025-06-29 12:15:12 +0100

    Reapply "feat: integrate EmailJS for booking modal"
    This reverts commit dcb0f74ec7f4969c8883ace5d93c250035462e2f.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -319,11 +319,18 @@ const CodeketLanding = () => {
 
             {/* CTAs */}
             <div className="flex flex-col sm:flex-row gap-4 justify-center items-center mb-12">
-              <button className="btn btn-primary btn-lg px-8">
+              <button
+                className="btn btn-primary btn-lg px-8"
+                onClick={() => setIsBookingModalOpen(true)}
+              >
                 <Camera className="w-5 h-5 mr-2" />
                 Book a Walkthrough Project
                 <ArrowRight className="w-5 h-5 ml-2" />
               </button>
+              <BookingModal
+                isOpen={isBookingModalOpen}
+                onClose={() => setIsBookingModalOpen(false)}
+              />
               <a href="#sample-tour" className="btn btn-outline btn-lg px-8">
                 <Play className="w-5 h-5 mr-2" />
                 View Sample Tour
