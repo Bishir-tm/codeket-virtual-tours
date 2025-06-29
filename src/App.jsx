b406c6ee7a4960045c76ab8f8b075565b0e2736d
bishir-tm <bishirtijjani247@gmail.com>
2025-06-29 10:41:24 +0100

    fix git config base url issues to use /

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,10 @@ import { Route, Routes } from "react-router-dom";
 
 const App = () => {
   return (
-    <div>
-      <Routes>
-        <Route path="/" Component={Home} />
-        <Route path="/*" Component={Home} />
-      </Routes>
-    </div>
+    <Routes>
+      <Route path="/" Component={Home} />
+      <Route path="/*" Component={Home} />
+    </Routes>
   );
 };
 
--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/codeket-virtual-tours/",
+  base: "/",
 
   plugins: [react()],
 });
